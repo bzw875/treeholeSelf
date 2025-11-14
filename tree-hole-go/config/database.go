bd8ef0d5998b6bcde996cca9629a9d4e09a7496f
班兆武 <banzhaowu_v@soulapp.cn>
2025-11-14 19:01:08 +0800

    up

--- tree-hole-go/config/database.go
+++ tree-hole-go/config/database.go
@@ -1,5 +1,9 @@
 package config
 
+import (
+	"os"
+)
+
 type DatabaseConfig struct {
 	Host     string
 	Port     int
@@ -8,12 +12,49 @@ type DatabaseConfig struct {
 	DBName   string
 }
 
+var (
+	hostName = "localhost"
+	port     = 3306
+	user     = "root"
+	dbName   = "jandan"
+)
+
 func GetDatabaseConfig() *DatabaseConfig {
+	env := os.Getenv("GO_ENV")
+	if env == "" {
+		env = "development"
+	}
+
+	switch env {
+	case "production":
+		return getProductionConfig()
+	default:
+		return getDevelopmentConfig()
+	}
+}
+
+func getDevelopmentConfig() *DatabaseConfig {
 	return &DatabaseConfig{
-		Host:     "localhost",
-		Port:     3306,
-		User:     "root",
+		Host:     hostName,
+		Port:     port,
+		User:     user,
 		Password: "J(87h9IU838UY*#H8yOJ",
-		DBName:   "jandan",
+		DBName:   dbName,
+	}
+}
+
+func getProductionConfig() *DatabaseConfig {
+
+	password := os.Getenv("DB_PASSWORD")
+	if password == "" {
+		password = "" // 生产环境建议必须设置密码
+	}
+
+	return &DatabaseConfig{
+		Host:     hostName,
+		Port:     port,
+		User:     user,
+		Password: password,
+		DBName:   dbName,
 	}
 }
